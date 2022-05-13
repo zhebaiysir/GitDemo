0bd37b69cab9a777f3836c081a5e0d3031ff628a
zhebaiysir <1351179662@qq.com>
2022-05-13 11:04:14 +0800

    implement:button add

--- app/src/main/java/com/sab/gitdemo/MainActivity.java
+++ app/src/main/java/com/sab/gitdemo/MainActivity.java
@@ -3,12 +3,27 @@ package com.sab.gitdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int num = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv_num = findViewById(R.id.tv_Add);
+
+        Button btn_add = findViewById(R.id.btn_add);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv_num.setText(++num);
+            }
+        });
     }
 }
